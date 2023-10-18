510f28ff6f3cebf64e25866bafa78777cf313be0
zong yang <billy62724@gmail.com>
2023-10-18 10:45:10 +0800

    first commit

---
+++ resources/views/products.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            哈哈哈哈哈
+    </title>
+    </head>
+        
+    
+    <body>
+        <h1>標題1</h1>
+        這是我的哈哈:<br/>
+        <a href="http://localhost:8000/about">關於(about) - 使用HTML A標籤</a>><br>
+        <a href="<?php echo route('demo.about'); ?>">關於(about) - 使用Larave1 的路由名稱</a><br>
+    </body> 
+</html>
